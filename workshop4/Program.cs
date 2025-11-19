ec93e60cae2f429c4d954cd6e971e5e63acf1eaa
sudankdk <sudankhadka345@gmail.com>
2025-11-19 11:54:13 +0545

    task 1 done

---
+++ workshop4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace workshop4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.Name = "aship Ram";
+        s1.Age = 20;
+
+        Student s2 = new Student();
+        s2.Name = "bishal Sita";
+        s2.Age = 19;
+
+        Console.WriteLine("Student 1:");
+        Console.WriteLine("Name: " + s1.Name);
+        Console.WriteLine("Age: " + s1.Age);
+        Console.WriteLine("School: " + Student.SchoolName);
+        
+        Console.WriteLine("Student 2:");
+        Console.WriteLine("Name: " + s2.Name);
+        Console.WriteLine("Age: " + s2.Age);
+        Console.WriteLine("School: " + Student.SchoolName);
+    }
+}
---
+++ workshop4/Student.cs
@@ -0,0 +1,9 @@
+namespace workshop4;
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+    public static string SchoolName = "Itahari International College";
+}
